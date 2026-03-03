0ba2fa9ddb4b405aa54f86d9c6aa7b0fb78778ca
gustavo9034-arch <gustavo.9034@colegioprogresso.g12.br>
2026-03-03 11:52:19 -0300

    Add files via upload

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'bcrypt'
+gem 'httparty'
+gem 'json'
+gem 'puma'
---
+++ app.rb
@@ -0,0 +1,83 @@
+require 'sinatra'
+require 'bcrypt'
+require 'httparty'
+require 'json'
+
+set :bind, '0.0.0.0'
+set :port, ENV['PORT'] || 4567
+
+SUPABASE_URL = "https://paizbjrlybeaxnziwfvo.supabase.co/rest/v1/usuarios"
+SUPABASE_KEY = "sb_publishable_dYbXv-7LanEDcPT-MW7Xbg_rX71b6F3"
+
+HEADERS = {
+  "apikey" => SUPABASE_KEY,
+  "Authorization" => "Bearer #{SUPABASE_KEY}",
+  "Content-Type" => "application/json",
+  "Prefer" => "return=representation"
+}
+
+headers = {
+  "apikey" => SUPABASE_KEY,
+  "Authorization" => "Bearer #{SUPABASE_KEY}",
+  "Content-Type" => "application/json"
+}
+
+get '/' do
+  redirect '/login'
+end
+
+get '/cadastro' do
+  erb :cadastro
+end
+
+post '/cadastro' do
+  usuario = params[:usuario]
+  senha = params[:senha]
+
+  if senha.length < 8
+    @erro = "Mínimo 8 caracteres"
+    return erb :cadastro
+  elsif !senha.match(/[0-9]/)
+    @erro = "Precisa ter 1 número"
+    return erb :cadastro
+  elsif !senha.match(/[A-Z]/)
+    @erro = "Precisa ter 1 letra maiúscula"
+    return erb :cadastro
+  end
+
+  senha_hash = BCrypt::Password.create(senha)
+
+  HTTParty.post(
+    SUPABASE_URL,
+    headers: HEADERS,
+    body: {
+      usuario: usuario,
+      senha_hash: senha_hash
+    }.to_json
+  )
+
+  redirect '/login'
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  usuario = params[:usuario]
+  senha = params[:senha]
+
+  response = HTTParty.get(
+    "#{SUPABASE_URL}?usuario=eq.#{usuario}",
+    headers: HEADERS
+  )
+
+  dados = JSON.parse(response.body)
+
+  if dados.any? && BCrypt::Password.new(dados[0]["senha_hash"]) == senha
+    erb :sucesso
+  else
+    @erro = "Login inválido"
+    erb :login
+  end
+end
